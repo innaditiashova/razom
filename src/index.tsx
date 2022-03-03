61b9390c17d4360c4b71b1ee2718915ba0b0ff35
Inna Ditiashova <inna.ditiashova@fiverr.com>
2022-03-03 13:29:46 +0200

    added firebase

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAOjg23Ska7kA5Vzwx3ntRiZDdZ_yAaEvo",
+    authDomain: "razom-c6314.firebaseapp.com",
+    projectId: "razom-c6314",
+    storageBucket: "razom-c6314.appspot.com",
+    messagingSenderId: "208972449486",
+    appId: "1:208972449486:web:8d20dcc294702b4ac7f29f",
+    measurementId: "G-8HPRW23Z57"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
